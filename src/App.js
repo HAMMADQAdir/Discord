f1385eafadb0a85fe8fcd2ff187792d96f6379d4
HAMMADQAdir <f1673673@gmail.com>
2024-06-23 11:18:41 +0530

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Card1 from './cards/card1';
+import Footer from './Footer';
 function App() {
   return (
    <>
  <Card1/>
+ <Footer/>
  
    </>
   );
--- src/cards/card1.jsx
+++ src/cards/card1.jsx
@@ -5,15 +5,15 @@ const Card1 = () => {
   const videoRef = useRef(null);
   return (
     <div className="h-[100%] w-[100%] flex items-center justify-center p-4 bg-blue-500">
-      <div className="rounded-3xl border-x-4 border-y-4 border-gray-600  bg-gray-400/10 flex flex-row ">
-        <div className=" mt-4 mr-3 w-52 font-sans p-10 text-white">
+      <div className="rounded-3xl border-x-4 border-y-4 border-gray-600  bg-gray-400/10 flex flex-col sm:flex-row">
+        <div className=" sm:mt-4 sm:mr-3 sm:w-52  font-sans p-10 text-white">
           <h1 className="font-extrabold text-xl">SEE WHO'S AROUND TO CHILL</h1>
-          <p>
+          <p className="m-4 sm:m-0 mr-16">
             See who's around, playing games, or just hanging out. For supported games, you can see what modes or characters your friends are playing and directly join up.
           </p>
         </div>
-        <div className="h-96 flex items-center justify-center m-8">
-          <video className=" align-middle rounded-3xl shadow-2xl  h-[100%]" ref={videoRef} muted autoPlay={true} loop>
+        <div className="sm:h-96 h-62 m-8 flex items-center justify-center">
+          <video className=" align-middle sm:rounded-3xl rounded-xl h-[100%]" ref={videoRef} muted autoPlay={true} loop>
           <source
             src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/6638c7e8907412911166f683_Discord_Website_Refresh_See%20Who_s%20Around-transcode.mp4"
             type="video/mp4"
