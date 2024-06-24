99cfa00c07f51adda9934cd203bd24e10e76ed4e
HAMMADQAdir <f1673673@gmail.com>
2024-06-24 10:51:31 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Card1 from './cards/card1';
 import Footer from './Footer';
+import NavigationTrail from './landingPage/navigationTrail';
 function App() {
   return (
    <>
  <Card1/>
- <Footer/>
- 
+
+<NavigationTrail/>
+<Footer/>
    </>
   );
 }
