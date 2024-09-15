d85a5dac30b09490a4683e25c1fe98dc5b9ce2a9
HAMMADQAdir <f1673673@gmail.com>
2024-09-15 12:09:05 +0530

    fixed sidenav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,16 @@ import Headers from './components/Header'
 import SideNav from './components/LandingPage/sideNav';
 import AddingServer from './components/LandingPage/addingServer';
 import MyServer from './components/MyServer';
+import Sidebar from './components/Sidebar';
 function App() {
   return (
     <>
        <div className="bg-[url('./assets/background_home.avif')] bg-cover bg-opacity-25 backdrop-blur-sm w-full h-full">
        {/* <Headers /> */}
-       <MyServer/>
+       {/* <MyServer/> */}
+       {/* <Sidebar/> */}
       
-        {/* <SideNav/>  */}
+        <SideNav/> 
    {/* <AddingServer/> */}
        {/* <Footer /> */}
        </div>
--- frontend/src/components/LandingPage/sideNav.jsx
+++ frontend/src/components/LandingPage/sideNav.jsx
@@ -3,21 +3,29 @@ import icon from "../LandingPage/icons8-discord-480.png";
 import profileIcon from "../LandingPage/hoq.jpg";
 import { CgAddR } from "react-icons/cg";
 import AddingServer from "./addingServer";
+import MyServer from "../MyServer";
+
+
 
 const SideNav = () => {
   const [activeIndex, setActiveIndex] = useState(null);
   const [hoveredIndex, setHoveredIndex] = useState(null);
-  const [isAddingServerOpen,setIsAddingServerOpen] = useState(false);
+  const [isMyServerOpen,setIsmyServerOpen] = useState(false);
   const handleClick = (index) => {
     setActiveIndex(index);
-    setIsAddingServerOpen(!isAddingServerOpen);
+    if(index == 1)
+    setIsmyServerOpen(true);
+  else
+  setIsmyServerOpen(false)
+    
 
   };
 
   return (
-    <div>
-    <aside className="fixed top-0 left-0 h-full w-30 bg-orange-400">
-      <nav className="h-full w-28 bg-black">
+
+    <div className="flex w-full h-full">
+    <aside className="fixed top-0 left-0 h-full w-28 bg-orange-400" style={{zIndex:100000}}>
+      <nav className="h-full w-28 bg-[#1E1F22]">
         <div className="flex items-center h-24" onClick={() => handleClick(0)}>
           <div
             className={`w-2 h-16 p-0 ${
@@ -44,10 +52,15 @@ const SideNav = () => {
                 : ""
             } ${activeIndex === 1 ? "bg-white rounded-r-3xl" : ""}`}></div>
           <img
-            src={profileIcon}
-            alt=""
-            className="transition-transform duration-300 ease-in-out transform hover:scale-105 ml-3 bg-clip h-16 w-16 rounded-full hover:rounded-2xl"
+            alt="My server"
+            className=" text-white transition-transform duration-300 ease-in-out transform hover:scale-105 ml-3 bg-clip h-16 w-16 rounded-full hover:rounded-2xl"
           />
+          {  hoveredIndex == 1 && (
+            // my server
+          <div className="transition-transform duration-300 ease-in-out transform absolute felx justify-center ml-14 translate-y-[200px] hover:scale-105 items-center bg-[#000000] p-2 rounded-lg shadow-md left-12 -top-4 whitespace-nowrap z-30">
+            <p className="text-xl inline-block text-[#c0c4d2] font-bold">Hammad's Server</p>
+            <div className="absolute w-4 h-4 bg-[#000000] top-2 left-[-8px] transform rotate-45"></div>
+          </div>)}
         </div>
         <div
           className="flex items-center h-24"
@@ -67,9 +80,10 @@ const SideNav = () => {
             className="transition-transform duration-300 ease-in-out transform hover:scale-105 ml-3 bg-clip h-16 w-16 rounded-full hover:rounded-2xl"
           />
           { activeIndex!==2 && hoveredIndex == 2 && (
-          <div className="transition-transform duration-300 ease-in-out transform absolute felx justify-center ml-14 translate-y-[300px] hover:scale-105 items-center bg-white p-2 rounded-lg shadow-md left-12 -top-4 whitespace-nowrap">
-            <p className="text-sm inline-block">add server</p>
-            <div className="absolute w-4 h-4 bg-white top-2 left-[-8px] transform rotate-45"></div>
+            // addin a server
+          <div className="transition-transform duration-300 ease-in-out transform absolute felx justify-center ml-14 translate-y-[300px] hover:scale-105 items-center bg-[#000000] p-2 rounded-lg shadow-md left-12 -top-4 whitespace-nowrap z-30">
+            <p className="text-xl inline-block text-[#c0c4d2] font-bold">Add a Server</p>
+            <div className="absolute w-4 h-4 bg-[#000000] top-2 left-[-8px] transform rotate-45"></div>
           </div>
           )}
           
@@ -91,12 +105,19 @@ const SideNav = () => {
             alt=""
             className="transition-transform duration-300 ease-in-out transform hover:scale-105 ml-3 bg-clip h-16 w-16 rounded-full hover:rounded-2xl"
           />
-        
+ { activeIndex!==3 && hoveredIndex == 3 && (
+            // discover Server
+          <div className="transition-transform duration-300 ease-in-out transform absolute felx justify-center ml-14 translate-y-[400px] hover:scale-105 items-center bg-[#000000] p-2 rounded-lg shadow-md left-12 -top-4 whitespace-nowrap z-30">
+            <p className="text-xl inline-block text-[#c0c4d2] font-bold">Explore Discoverable Servers</p>
+            <div className="absolute w-4 h-4 bg-[#000000] top-2 left-[-8px] transform rotate-45"></div>
+          </div>)}
         </div>
       </nav>
     
     </aside>
-    {isAddingServerOpen && <AddingServer/>}
+    <div className="flex-grow ml-28">
+    {isMyServerOpen && <MyServer/>}
+    </div>
    </div>
 
   );
